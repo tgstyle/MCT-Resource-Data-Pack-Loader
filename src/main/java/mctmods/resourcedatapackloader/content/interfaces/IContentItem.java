f7b119903a391215c1b6f2326fad0c1b671ca52e
tgstyle <5832709+tgstyle@users.noreply.github.com>
2026-08-03 18:36:20 -0600

    Stub for 1.20.1

--- src/main/java/mctmods/resourcedatapackloader/content/interfaces/IContentItem.java
+++
@@ -1,7 +0,0 @@
-package mctmods.resourcedatapackloader.content.interfaces;
-
-import mctmods.resourcedatapackloader.content.def.ItemDef;
-
-public interface IContentItem {
-    ItemDef getDef();
-}
--- src/main/java/mctmods/resourcedatapackloader/content/interfaces/IContentShape.java
+++
@@ -1,9 +0,0 @@
-package mctmods.resourcedatapackloader.content.interfaces;
-
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import java.util.Random;
-
-public interface IContentShape {
-    boolean generate(World world, Random random, BlockPos origin);
-}
